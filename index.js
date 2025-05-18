28170424e70f03bc5cbaa10994353f1dbd6f8f81
Priyesh Soni <priyesh9792@gmail.com>
2025-05-18 08:13:22 +0530

    Set up Prisma, Express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
+require("dotenv").config();
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("server running......."));
